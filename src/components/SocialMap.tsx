98a76b7b59b880c4f9400e28615b7b6d017db6b0
Wells Wu <wellswu@WellsdeMacBook-Air-5.local>
2026-02-21 20:54:59 +0800

    style: move map filters to top right to avoid banner overlap

--- src/components/SocialMap.tsx
+++ src/components/SocialMap.tsx
@@ -157,7 +157,7 @@ export default function SocialMap({ className }: { className?: string }) {
 
             {/* Category Filters Overlay - Simplified */}
             {!isPinningMode && (
-                <div className="absolute top-6 left-1/2 -translate-x-1/2 z-20 flex flex-wrap justify-center gap-2 px-4 pointer-events-auto">
+                <div className="absolute top-6 right-4 z-20 flex flex-wrap justify-end gap-2 px-4 pointer-events-auto">
                     {(['all', 'student'] as const).map((type) => (
                         <button
                             key={type}
